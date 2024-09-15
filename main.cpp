da9b3c95a580a05c3ac0ae3d904a92727e78411b
malkivska <v.a.malkivska@student.khai.edu>
2024-09-15 17:08:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std; //instead of prefix std::
+int main() //header
+{ //begin
+    cout << "Hello World!";
+   return 0; //return to OS 
+} //end
